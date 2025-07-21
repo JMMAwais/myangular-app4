33c9922a0e96d212da9dd983e2fcce333b7a6e8c
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-07-21 14:32:00 +0500

    add sdiebar component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 
 @Component({
@@ -16,7 +17,8 @@ import { Router } from '@angular/router';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
-    RouterModule],
+    RouterModule,
+  SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
