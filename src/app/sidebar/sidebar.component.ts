15454fcba7638a95aa6435fd7e5c4c7fb097ccea
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-07-21 18:07:40 +0500

    resolved error

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+constructor(private authService:AuthService){}
+  logout() {
+    this.authService.logout();
+  }
 }
