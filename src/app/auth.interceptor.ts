45cfbf2011892a507ed59c18776f4c17a9147f6a
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-11-24 12:20:36 +0500

    modify interceptor 3

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -2,7 +2,6 @@ import {
   HttpInterceptorFn,
   HttpRequest,
   HttpHandlerFn,
-  HttpEvent,
   HttpErrorResponse
 } from '@angular/common/http';
 import { inject } from '@angular/core';
@@ -11,19 +10,51 @@ import { catchError, switchMap, throwError } from 'rxjs';
 import { AuthService } from './services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
-  const cloned = req.clone({ withCredentials: true });
+  
   const http = inject(HttpClient);
   const authService = inject(AuthService);
 
-  return next(cloned).pipe(
+  // ⭐ GET ACCESS TOKEN
+  const token = localStorage.getItem('access_token');
+
+  // ⭐ ADD TOKEN TO HEADER
+  let authReq = req;
+
+  if (token) {
+    authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      },
+      withCredentials: true
+    });
+  } else {
+    authReq = req.clone({ withCredentials: true });
+  }
+
+  // ⭐ HANDLE REFRESH LOGIC
+  return next(authReq).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401 && !req.url.includes('refresh-token')) {
-        // Try refreshing token
 
         return authService.refreshToken().pipe(
-          switchMap(() => next(cloned)),
+          switchMap((res) => {
+
+            // ⭐ NEW TOKEN SAVE
+            localStorage.setItem("access_token", res.access_token);
+
+            // ⭐ REPEAT REQUEST WITH NEW TOKEN
+            const newReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${res.access_token}`
+              },
+              withCredentials: true
+            });
+
+            return next(newReq);
+          }),
           catchError(refreshError => {
             console.error('Refresh failed', refreshError);
+            authService.logout();
             return throwError(() => refreshError);
           })
         );
